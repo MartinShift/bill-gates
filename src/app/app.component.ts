36e0c95d35536af9baa9e3f000246e0e137eaf1b
MartinShift <saprykin.martin@gmail.com>
2024-03-24 11:56:41 +0100

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'bill-gates';
+
+  image='https://upload.wikimedia.org/wikipedia/commons/1/19/Bill_Gates_June_2015.jpg';
+
+  quotes = [
+    'If you think your teacher is tough, wait until you get a boss.',
+    'Life is not fair — get used to it!',
+    'Success is a lousy teacher. This seduces smart people into thinking they can’t lose.',
+    'Be nice to nerds.'
+  ];
+
+  bio = 'Bill Gates is an American business magnate, software developer, investor, author, and philanthropist. He is a co-founder of Microsoft Corporation.';
+
+  resources = [
+    { name: 'Wikipedia', url: 'https://en.wikipedia.org/wiki/Bill_Gates' },
+    { name: 'Gates Foundation', url: 'https://www.gatesfoundation.org/' },
+    { name: 'Gates Notes', url: 'https://www.gatesnotes.com/' },
+    { name: 'Twitter', url: 'https://twitter.com/billgates' }
+  ];
 }
